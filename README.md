cbd47726a9785939aa051e02df1d12048ba361e2
Steven M. Cohn <steven_cohn@waters.com>
2018-05-28 10:13:30 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # YellowCursors
 Yellow mouse cursor theme for Windows
+
+![alt text](Cursors.jpg "Yellow Cursors")
+
+## Installation
+
+Installation is simple. Just copy the cursors from this repo into C:\Windows\Cursors.
+
+But if you'd like to automate the process, here's a script:
+
+```powershell
+$src='https://github.com/stevencohn/YellowCursors/archive/master.zip'
+$zip="${env:TEMP}\YellowCursors.zip"
+$exp="${env:TEMP}\YellowCursors-master"
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $src -OutFile $zip
+Expand-Archive -Path $zip -DestinationPath "${env:TEMP}\" -Force
+Copy-Item "$exp\*.ani" C:\Windows\Cursors\ -Force
+Copy-Item "$exp\*.cur" C:\Windows\Cursors\ -Force
+Remove-Item $zip -Confirm:$False
+Remove-Item $exp -Recurse -Force -Confirm:$False
+```
+
+Then run the install.ps1 script
+
+```powershell
+.\install.ps1
+```
+
+Of course, run both of these as administrator.
+
+Notice that install.ps1 forces a dynamic reload of cursors.
+This should work without requiring a logout/login or reboot.
+
+Enjoy!
---
+++ install.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+Install the yellow mouse cursor theme.
+#>
+
+# create Yellow scheme
+$0 = 'HKCU:\ControlPanel\Cursors'
+$p = '%SYSTEMROOT%\Cursors'
+
+New-ItemProperty -Path "$0\Schemes" -Force -Name 'Yellow' -Value "$p\yellow_arrow.cur,$p\yellow_helpsel.cur,$p\yellow_working.ani,$p\aero_busy.ani,,,$p\aero_pen.cur,$p\aero_unavail.cur,$p\yellow_ns.cur,$p\yellow_ew.cur,$p\yellow_nwse.cur,$p\yellow_nesw.cur,$p\yellow_move.cur,$p\yellow_up.cur,$p\yellow_link.cur"
+
+# apply Yellow scheme
+New-ItemProperty $0 -Value 'Yellow' -Force
+New-ItemProperty $0 -Name 'AppStarting' -Value "$0\yellow_working.ani" -Force
+New-ItemProperty $0 -Name 'Arrow' -Value "$0\yellow_arrow.cur" -Force
+New-ItemProperty $0 -Name 'Crosshair' -Value '' -Force
+New-ItemProperty $0 -Name 'Hand' -Value "$0\yellow_link.cur" -Force
+New-ItemProperty $0 -Name 'Help' -Value "$0\yellow_helpsel.cur" -Force
+New-ItemProperty $0 -Name 'IBeam' -Value '' -Force
+New-ItemProperty $0 -Name 'No' -Value "$0\yellow_aero_unavail.cur" -Force
+New-ItemProperty $0 -Name 'NWPen' -Value "$0\aero_pen.cur" -Force
+New-ItemProperty $0 -Name 'SchemeSource' -Value 2 -Type dword -Force
+New-ItemProperty $0 -Name 'SizeAll' -Value "$0\yellow_move.cur" -Force
+New-ItemProperty $0 -Name 'SizeNESW' -Value "$0\yellow_nesw.cur" -Force
+New-ItemProperty $0 -Name 'SizeNS' -Value "$0\yellow_ns.cur" -Force
+New-ItemProperty $0 -Name 'SizeNWSE' -Value "$0\yellow_nwse.cur" -Force
+New-ItemProperty $0 -Name 'SizeWE' -Value "$0\yellow_ew.cur" -Force
+New-ItemProperty $0 -Name 'UpArrow' -Value "$0\yellow_up.cur" -Force
+New-ItemProperty $0 -Name 'Wait' -Value "$0\aero_busy.ani" -Force
+
+# reload system cursors
+$cs = @'
+[DllImport("user32.dll", SetLastError = true, EntryPoint = "SystemParametersInfo")]
+[return: MarshalAs(UnmanagedType.Bool)]
+public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, bool pvParam, uint fWinIni);
+'@
+ 
+$SPI_SETCURSORS = 0x0057
+$CursorRefresh = Add-Type -MemberDefinition $cs -Name WinAPICall -Namespace SystemParamInfo –PassThru
+$CursorRefresh::SystemParametersInfo($SPI_SETCURSORS, 0, $null, 0)
