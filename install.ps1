a54d0bbc0a5a9d6ed45253307cc9c7b27d7abe95
Steven Cohn <steven_cohn@waters.com>
2018-05-29 10:42:44 -0400

    fixed to install.ps1

--- install.ps1
+++ install.ps1
@@ -1,32 +1,33 @@
-<#
+﻿<# 
 .SYNOPSIS
 Install the yellow mouse cursor theme.
 #>
 
 # create Yellow scheme
-$0 = 'HKCU:\ControlPanel\Cursors'
-$p = '%SYSTEMROOT%\Cursors'
+$0 = 'HKCU:\Control Panel\Cursors'
+$p = '%SystemRoot%\cursors'
 
-New-ItemProperty -Path "$0\Schemes" -Force -Name 'Yellow' -Value "$p\yellow_arrow.cur,$p\yellow_helpsel.cur,$p\yellow_working.ani,$p\aero_busy.ani,,,$p\aero_pen.cur,$p\aero_unavail.cur,$p\yellow_ns.cur,$p\yellow_ew.cur,$p\yellow_nwse.cur,$p\yellow_nesw.cur,$p\yellow_move.cur,$p\yellow_up.cur,$p\yellow_link.cur"
+if (!(Test-Path "$0\Schemes")) { $null = New-Item -Path $0 -Name 'Schemes' -Force }
+$null = New-ItemProperty -Path "$0\Schemes" -Force -Name 'Yellow' -Value "$p\yellow_arrow.cur,$p\yellow_helpsel.cur,$p\yellow_working.ani,$p\aero_busy.ani,,,$p\aero_pen.cur,$p\aero_unavail.cur,$p\yellow_ns.cur,$p\yellow_ew.cur,$p\yellow_nwse.cur,$p\yellow_nesw.cur,$p\yellow_move.cur,$p\yellow_up.cur,$p\yellow_link.cur"
 
 # apply Yellow scheme
-New-ItemProperty $0 -Value 'Yellow' -Force
-New-ItemProperty $0 -Name 'AppStarting' -Value "$0\yellow_working.ani" -Force
-New-ItemProperty $0 -Name 'Arrow' -Value "$0\yellow_arrow.cur" -Force
-New-ItemProperty $0 -Name 'Crosshair' -Value '' -Force
-New-ItemProperty $0 -Name 'Hand' -Value "$0\yellow_link.cur" -Force
-New-ItemProperty $0 -Name 'Help' -Value "$0\yellow_helpsel.cur" -Force
-New-ItemProperty $0 -Name 'IBeam' -Value '' -Force
-New-ItemProperty $0 -Name 'No' -Value "$0\yellow_aero_unavail.cur" -Force
-New-ItemProperty $0 -Name 'NWPen' -Value "$0\yellow_pen.cur" -Force
-New-ItemProperty $0 -Name 'SchemeSource' -Value 2 -Type dword -Force
-New-ItemProperty $0 -Name 'SizeAll' -Value "$0\yellow_move.cur" -Force
-New-ItemProperty $0 -Name 'SizeNESW' -Value "$0\yellow_nesw.cur" -Force
-New-ItemProperty $0 -Name 'SizeNS' -Value "$0\yellow_ns.cur" -Force
-New-ItemProperty $0 -Name 'SizeNWSE' -Value "$0\yellow_nwse.cur" -Force
-New-ItemProperty $0 -Name 'SizeWE' -Value "$0\yellow_ew.cur" -Force
-New-ItemProperty $0 -Name 'UpArrow' -Value "$0\yellow_up.cur" -Force
-New-ItemProperty $0 -Name 'Wait' -Value "$0\aero_busy.ani" -Force
+Set-Item $0 -Value 'Yellow' -Force
+$null = New-ItemProperty $0 -Name 'AppStarting' -Value "$p\yellow_working.ani" -Force
+$null = New-ItemProperty $0 -Name 'Arrow' -Value "$p\yellow_arrow.cur" -Force
+$null = New-ItemProperty $0 -Name 'Crosshair' -Value '' -Force
+$null = New-ItemProperty $0 -Name 'Hand' -Value "$p\yellow_link.cur" -Force
+$null = New-ItemProperty $0 -Name 'Help' -Value "$p\yellow_helpsel.cur" -Force
+$null = New-ItemProperty $0 -Name 'IBeam' -Value '' -Force
+$null = New-ItemProperty $0 -Name 'No' -Value "$p\yellow_aero_unavail.cur" -Force
+$null = New-ItemProperty $0 -Name 'NWPen' -Value "$p\yellow_pen.cur" -Force
+$null = New-ItemProperty $0 -Name 'Scheme Source' -Value 2 -Type dword -Force
+$null = New-ItemProperty $0 -Name 'SizeAll' -Value "$p\yellow_move.cur" -Force
+$null = New-ItemProperty $0 -Name 'SizeNESW' -Value "$p\yellow_nesw.cur" -Force
+$null = New-ItemProperty $0 -Name 'SizeNS' -Value "$p\yellow_ns.cur" -Force
+$null = New-ItemProperty $0 -Name 'SizeNWSE' -Value "$p\yellow_nwse.cur" -Force
+$null = New-ItemProperty $0 -Name 'SizeWE' -Value "$p\yellow_ew.cur" -Force
+$null = New-ItemProperty $0 -Name 'UpArrow' -Value "$p\yellow_up.cur" -Force
+$null = New-ItemProperty $0 -Name 'Wait' -Value "$p\aero_busy.ani" -Force
 
 # reload system cursors
 $cs = @'
